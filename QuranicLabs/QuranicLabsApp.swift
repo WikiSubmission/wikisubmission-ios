a7276d1e48e53368e22adbd34cb9814126c3fb7e
Sarim <sarimyx@proton.me>
2025-09-24 13:21:43 -0600

    Initial Commit

---
+++ QuranicLabs/QuranicLabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranicLabsApp.swift
+//  QuranicLabs
+//
+//  Created by Sarim on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct QuranicLabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
