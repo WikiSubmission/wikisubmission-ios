2f6f8f76f0e1ba74bb510f72d5bc635567837fd3
Sarim <sarimyx@proton.me>
2025-10-02 17:52:27 -0600

    unused key

--- QuranicLabs/AppStorage/QuranBookmarksKeys.swift
+++ QuranicLabs/AppStorage/QuranBookmarksKeys.swift
@@ -3,5 +3,4 @@ import Foundation
 
 extension Defaults.Keys {
     static let bookmarks = Key<[Types.Bookmarks.Bookmark]>("bookmarks", default: [])
-    static let bookmarked_synced = Key<Bool>("synced", default: false)
 }
